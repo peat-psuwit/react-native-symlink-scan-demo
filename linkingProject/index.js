9711207f62ad29f64c75505efbcaa50374dd53c3
Ratchanan Srirattanamet <peathot@hotmail.com>
2018-10-12 00:01:58 +0700

    Import something from linked-package to linkingProject

--- linkingProject/index.js
+++ linkingProject/index.js
@@ -4,4 +4,8 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+import linkedString from 'linked-package';
+
+console.log(linkedString);
+
 AppRegistry.registerComponent(appName, () => App);
